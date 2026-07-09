ac63fe3c62c9189afda6552c5b67354cfa4bf578
CJ Kim <cjoak1028@gmail.com>
2026-07-09 11:41:52 -0400

    Add back button, its functionality, and refactor go next step logic

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ const planForm = document.querySelector("#plan-form");
 const addOnsForm = document.querySelector("#add-ons-form");
 const summaryForm = document.querySelector("#summary-form");
 const nextBtn = document.querySelector("#next-btn");
+const backBtn = document.querySelector("#back-btn");
+const confirmBtn = document.querySelector("#confirm-btn");
 
 let curStep = 1;
 
-nextBtn.addEventListener("click", () => {
-  let nextStep = curStep + 1;
+const goNextStep = (step, reverse = false) => {
+  const nextStep = reverse ? step - 1 : step + 1;
 
-  const activePanel = document.querySelector(`[data-step-panel="${curStep}"]`);
+  const activePanel = document.querySelector(`[data-step-panel="${step}"]`);
   const nextPanel = document.querySelector(`[data-step-panel="${nextStep}"]`);
   const activeStepIndicator = document.querySelector(
-    `[data-step-item="${curStep}"]`,
+    `[data-step-item="${step}"]`,
   );
   const nextStepIndicator = document.querySelector(
     `[data-step-item="${nextStep}"]`,
@@ -24,10 +26,37 @@ nextBtn.addEventListener("click", () => {
   activeStepIndicator.removeAttribute("data-active");
   nextStepIndicator.setAttribute("data-active", "");
 
+  return nextStep;
+};
+
+nextBtn.addEventListener("click", () => {
+  const nextStep = goNextStep(curStep);
+
   curStep = nextStep;
 
-  // TODO: Remove when back button is implemented
-  if (curStep === 4) {
+  if (nextStep === 4) {
     nextBtn.classList.add("hidden");
+    confirmBtn.classList.remove("hidden");
+  }
+
+  if (nextStep > 1) {
+    backBtn.classList.remove("hidden");
+    backBtn.removeAttribute("data-hidden-back");
+  }
+});
+
+backBtn.addEventListener("click", () => {
+  const nextStep = goNextStep(curStep, true);
+
+  curStep = nextStep;
+
+  if (nextStep === 1) {
+    backBtn.classList.add("hidden");
+    backBtn.setAttribute("data-hidden-back", "");
+  }
+
+  if (nextStep === 3) {
+    nextBtn.classList.remove("hidden");
+    confirmBtn.classList.add("hidden");
   }
 });
