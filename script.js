2edaeac77d7355b8060e8dffe994ec9888f14bce
CJ Kim <cjoak1028@gmail.com>
2026-07-13 16:47:20 -0400

    Implement functionality of billing cycle toggle

--- script.js
+++ script.js
@@ -4,8 +4,50 @@ const confirmBtn = document.querySelector("#confirm-btn");
 const billingToggle = document.querySelector("#billing-toggle");
 const monthlyOption = document.querySelector("#monthly-option");
 const yearlyOption = document.querySelector("#yearly-option");
+const arcadePrice = document.querySelector("#arcade-price");
+const advancedPrice = document.querySelector("#advanced-price");
+const proPrice = document.querySelector("#pro-price");
+const onlinePrice = document.querySelector("#online-price");
+const storagePrice = document.querySelector("#storage-price");
+const profilePrice = document.querySelector("#profile-price");
 
 let curStep = 1;
+
+const state = {
+  billingCycle: "monthly",
+  selectedPlan: "arcade",
+  selectedAddons: [],
+};
+
+const plans = {
+  arcade: {
+    mo: 9,
+    yr: 90,
+  },
+  advanced: {
+    mo: 12,
+    yr: 120,
+  },
+  pro: {
+    mo: 15,
+    yr: 150,
+  },
+};
+
+const addons = {
+  online: {
+    mo: 1,
+    yr: 10,
+  },
+  storage: {
+    mo: 2,
+    yr: 20,
+  },
+  profile: {
+    mo: 2,
+    yr: 20,
+  },
+};
 
 const goNextStep = (step, reverse = false) => {
   const nextStep = reverse ? step - 1 : step + 1;
@@ -26,6 +68,27 @@ const goNextStep = (step, reverse = false) => {
   nextStepIndicator.setAttribute("data-active", "");
 
   return nextStep;
+};
+
+const renderPlans = (cycle = "mo") => {
+  arcadePrice.textContent = `$${plans.arcade[cycle]}/${cycle}`;
+  advancedPrice.textContent = `$${plans.advanced[cycle]}/${cycle}`;
+  proPrice.textContent = `$${plans.pro[cycle]}/${cycle}`;
+};
+
+const renderAddons = (cycle = "mo") => {
+  onlinePrice.textContent = `+$${addons.online[cycle]}/${cycle}`;
+  storagePrice.textContent = `+$${addons.storage[cycle]}/${cycle}`;
+  profilePrice.textContent = `+$${addons.profile[cycle]}/${cycle}`;
+};
+
+const render = (cycle = "mo") => {
+  renderPlans(cycle);
+  renderAddons(cycle);
+};
+
+const init = () => {
+  render();
 };
 
 nextBtn.addEventListener("click", () => {
@@ -63,7 +126,19 @@ backBtn.addEventListener("click", () => {
 billingToggle.addEventListener("click", () => {
   if (monthlyOption.checked) {
     yearlyOption.checked = true;
+    render("yr");
   } else {
     monthlyOption.checked = true;
+    render("mo");
   }
 });
+
+monthlyOption.addEventListener("change", () => {
+  render("mo");
+});
+
+yearlyOption.addEventListener("change", () => {
+  render("yr");
+});
+
+init();
